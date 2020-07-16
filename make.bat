71b47ec40911f25bbc8a4819f9fbcf8a73b11c03
john-lay <john_lay@hotmail.co.uk>
2020-07-16 17:49:16 +0100

    Move make file to root and refactor

---
+++ make.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set project="lesson1"
+if exist %project%\main.gb del %project%\main.gb
+if exist %project%\main.o del %project%\main.o
+if exist %project%\main.lst del %project%\main.lst
+if exist %project%\main.map del %project%\main.map
+if exist %project%\main.sym del %project%\main.sym
+
+:begin
+echo assembling...
+	c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o %project%\main.o %project%\main.c
+	if errorlevel 1 goto end
+echo linking...
+	c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o %project%\main.gb %project%\main.o
+	if errorlevel 1 goto end
+:end
