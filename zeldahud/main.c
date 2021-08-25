eb636a7ea5e9887474f2967d06b5a8eddf9a2bda
john-lay <john_lay@hotmail.co.uk>
2021-08-25 20:52:47 +0100

    Generate a scene using ggbgfx

--- zeldahud/main.c
+++ zeldahud/main.c
@@ -2,10 +2,17 @@
 #include <stdio.h>
 #include "zeldaTiles.c"
 #include "zeldaMap.c"
+#include "tileset.c"
+#include "tilemap.c"
 
 void main() {
-    set_bkg_data(0, 15, zeldaTiles);
-    set_bkg_tiles(0, 0, 20, 1, zeldaMap);
+    // load the background tiles (vision henge scene)
+    set_bkg_data(0, 32, mytileset); // there are 32 tiles 
+    set_bkg_tiles(0, 0, 20, 18, mytilemap);
+
+    // load the windows tiles
+    // set_bkg_data(0, 15, zeldaTiles);
+    // set_bkg_tiles(0, 0, 20, 1, zeldaMap);
 
     SHOW_BKG;
     DISPLAY_ON;
