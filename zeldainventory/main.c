134545f372eb0248cb796e698190634b87192264
john-lay <john_lay@hotmail.co.uk>
2021-12-20 22:40:48 +0000

    Scroll through treasures with up/down keys
    Init weapons/treasures obeys scroll offset

--- zeldainventory/main.c
+++ zeldainventory/main.c
@@ -3,16 +3,18 @@
 #include "tilemap.c"
 #include "zeldaTypes.c"
 
-UINT8 weaponScrollOffset = 0;
+const UINT8 maxItemsOnScreen = 6;
 ZELDA_TREASURES equipped = ZELDA_TREASURE_UNDEFINED; // this will be read from GB Studio
 UINT8 highlighted = 0;                               // if the equipped treasure is visible this is set to 1-6
-UINT8 totalTreasuresFound = 10;                      // calculated at the same time as treasures[] based on GB Studio interrogation
-UINT8 totalWeaponsFound = 18;
 UINT8 slot = 0;
-const UINT8 maxItemsOnScreen = 6;
 
-// tile index starts at 54 and moves 4 tiles for each new item
-UINT8 tileIndex = 54;
+UINT8 weaponScrollOffset = 0;
+UINT8 totalWeaponsFound = 19; // calculated at the same time as treasures[] based on GB Studio interrogation
+UINT8 weaponTileIndex = 54;   // tile index starts at 54 and moves 4 tiles for each new item
+
+UINT8 treasureScrollOffset = 0;
+UINT8 totalTreasuresFound = 29;
+UINT8 treasureTileIndex = 86; // tile index starts at 86 and moves 4 tiles for each new item
 
 unsigned char treasureContainer[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                                      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
@@ -20,37 +22,71 @@ unsigned char treasureContainer[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0
 unsigned char weaponContainer[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
-// this needs to be dynamically populated based on GB Studio variable interrogation
-ZELDA_TREASURES treasures[10] = {ZELDA_TREASURE_PLANK,
+// TODO: solve large arrays > 32 elements look up causing issues.
+ZELDA_TREASURES treasures[29] = {ZELDA_TREASURE_BLACKORB,
+                                 ZELDA_TREASURE_BONE,
+                                 ZELDA_TREASURE_BOUQUET,
+                                 ZELDA_TREASURE_CANDLE,
+                                 ZELDA_TREASURE_COAL,
+                                 ZELDA_TREASURE_COMPASS1,
+                                 //  ZELDA_TREASURE_COMPASS2,
+                                 //  ZELDA_TREASURE_COMPASS3,
+                                 //  ZELDA_TREASURE_COMPASS4,
+                                 //  ZELDA_TREASURE_COMPASS5,
+                                 //  ZELDA_TREASURE_COMPASS6,
+                                 //  ZELDA_TREASURE_COMPASS7,
+                                 ZELDA_TREASURE_DIAMOND,
+                                 ZELDA_TREASURE_EMPTYBOTTLE,
+                                 ZELDA_TREASURE_FLUTE,
+                                 ZELDA_TREASURE_GOLDENBOOTS,
+                                 ZELDA_TREASURE_HARP,
+                                 ZELDA_TREASURE_KEY,
+                                 ZELDA_TREASURE_KNIFE,
+                                 ZELDA_TREASURE_LADDER,
+                                 ZELDA_TREASURE_LIFEPOTION,
+                                 ZELDA_TREASURE_MAGICSHIELD,
+                                 ZELDA_TREASURE_MAP1,
+                                 //  ZELDA_TREASURE_MAP2,
+                                 //  ZELDA_TREASURE_MAP3,
+                                 //  ZELDA_TREASURE_MAP4,
+                                 //  ZELDA_TREASURE_MAP5,
+                                 //  ZELDA_TREASURE_MAP6,
+                                 //  ZELDA_TREASURE_MAP7,
+                                 ZELDA_TREASURE_PLANK,
                                  ZELDA_TREASURE_RAFT,
                                  ZELDA_TREASURE_REDBOOTS,
+                                 ZELDA_TREASURE_REDBOW,
                                  ZELDA_TREASURE_REPELLENT,
                                  ZELDA_TREASURE_RUG,
+                                 ZELDA_TREASURE_RUPEE,
                                  ZELDA_TREASURE_SALTCELLAR,
                                  ZELDA_TREASURE_TICKETA,
                                  ZELDA_TREASURE_TICKETB,
                                  ZELDA_TREASURE_VIALOFWIND,
                                  ZELDA_TREASURE_WATERBOTTLE};
 
-ZELDA_WEAPONS weapons[19] = {ZELDA_WEAPON_BOOMERANG,
-                             ZELDA_WEAPON_BOWANDARROW,
-                             ZELDA_WEAPON_BROADSWORD,
-                             ZELDA_WEAPON_CALM,
-                             ZELDA_WEAPON_DAGGER,
-                             ZELDA_WEAPON_GOLDNECKLACE,
-                             ZELDA_WEAPON_FEATHER,
-                             ZELDA_WEAPON_FIRESTORM,
-                             ZELDA_WEAPON_HAMMER,
-                             ZELDA_WEAPON_JADEAMULET,
-                             ZELDA_WEAPON_JADERING,
-                             ZELDA_WEAPON_JOUST,
-                             ZELDA_WEAPON_NOISE,
-                             ZELDA_WEAPON_PYROS,
-                             ZELDA_WEAPON_RINGSOFFIRE,
-                             ZELDA_WEAPON_ROARSTICK,
-                             ZELDA_WEAPON_SHORTAXE,
-                             ZELDA_WEAPON_TURQUOISERING,
-                             ZELDA_WEAPON_WAND};
+// this needs to be dynamically populated based on GB Studio variable interrogation
+ZELDA_WEAPONS weapons[19] = {
+    ZELDA_WEAPON_WAND,
+    ZELDA_WEAPON_BOOMERANG,
+    ZELDA_WEAPON_BOWANDARROW,
+    ZELDA_WEAPON_BROADSWORD,
+    ZELDA_WEAPON_CALM,
+    ZELDA_WEAPON_DAGGER,
+    ZELDA_WEAPON_GOLDNECKLACE,
+    ZELDA_WEAPON_FEATHER,
+    ZELDA_WEAPON_FIRESTORM,
+    ZELDA_WEAPON_HAMMER,
+    ZELDA_WEAPON_JADEAMULET,
+    ZELDA_WEAPON_JADERING,
+    ZELDA_WEAPON_JOUST,
+    ZELDA_WEAPON_NOISE,
+    ZELDA_WEAPON_PYROS,
+    ZELDA_WEAPON_RINGSOFFIRE,
+    ZELDA_WEAPON_ROARSTICK,
+    ZELDA_WEAPON_SHORTAXE,
+    ZELDA_WEAPON_TURQUOISERING,
+};
 
 UBYTE GetBit(UINT8 byte, UINT8 bit)
 {
@@ -148,7 +184,7 @@ void addWeaponToVram(ZELDA_WEAPONS weapon, UINT8 tileIndex)
         set_bkg_data(tileIndex, 4, turquoiseRingTileset);
         break;
     case ZELDA_WEAPON_WAND:
-        // set_bkg_data(tileIndex, 4, );
+        set_bkg_data(tileIndex, 4, wandTileset);
         break;
     default:
         break;
@@ -157,17 +193,20 @@ void addWeaponToVram(ZELDA_WEAPONS weapon, UINT8 tileIndex)
 
 void initialiseWeapons()
 {
-    tileIndex = 54;
+    weaponTileIndex = 54;
     slot = 0;
     for (UINT8 i = 0; i < 8; i++)
     {
-        addWeaponToVram(weapons[i], tileIndex);
-        tileIndex += 4;
-
-        if (i < maxItemsOnScreen)
+        if (i < totalWeaponsFound)
         {
-            placeItemOnScreen(weaponSlots[i], weaponContainer, slot);
-            slot += 2;
+            addWeaponToVram(weapons[weaponScrollOffset + i], weaponTileIndex);
+            weaponTileIndex += 4;
+
+            if (i < maxItemsOnScreen)
+            {
+                placeItemOnScreen(weaponSlots[i], weaponContainer, slot);
+                slot += 2;
+            }
         }
     }
 }
@@ -216,18 +255,18 @@ void ScrollWeaponsLeft()
         weaponScrollOffset--;
 
         // calculate tile index
-        tileIndex = 54;
-        tileIndex += (weaponScrollOffset)*4;
+        weaponTileIndex = 54;
+        weaponTileIndex += (weaponScrollOffset)*4;
 
         // check if we've written to the end of 8*4 tile slots reserved for weapons
         if (weaponScrollOffset >= 8)
         {
-            tileIndex = 54;
-            tileIndex += (weaponScrollOffset - 8) * 4;
+            weaponTileIndex = 54;
+            weaponTileIndex += (weaponScrollOffset - 8) * 4;
         }
 
         // add a new tile to VRAM
-        addWeaponToVram(weapons[weaponScrollOffset], tileIndex);
+        addWeaponToVram(weapons[weaponScrollOffset], weaponTileIndex);
         placeItemOnScreen(weaponSlots[0], weaponContainer, 0);
 
         updateWeapons();
@@ -241,28 +280,289 @@ void ScrollWeaponsRight()
         weaponScrollOffset++;
 
         // calculate the tile index
-        tileIndex = 54;
+        weaponTileIndex = 54;
 
         if (weaponScrollOffset > 2)
         {
-            tileIndex += (weaponScrollOffset - 2) * 4;
+            weaponTileIndex += (weaponScrollOffset - 2) * 4;
         }
 
         // check if we've written to the end of 8*4 tile slots reserved for weapons
         if (weaponScrollOffset >= 10)
         {
-            tileIndex = 54;
-            tileIndex += (weaponScrollOffset - 10) * 4;
+            weaponTileIndex = 54;
+            weaponTileIndex += (weaponScrollOffset - 10) * 4;
         }
 
         // add a new tile to VRAM
         if (weaponScrollOffset > 1)
         {
-            addWeaponToVram(weapons[weaponScrollOffset + 6], tileIndex);
+            addWeaponToVram(weapons[weaponScrollOffset + 6], weaponTileIndex);
             placeItemOnScreen(weaponSlots[0], weaponContainer, 0);
         }
 
         updateWeapons();
+    }
+}
+
+void addTreasureToVram(ZELDA_TREASURES treasure, UINT8 tileIndex)
+{
+    switch (treasure)
+    {
+    case ZELDA_TREASURE_BLACKORB:
+        set_bkg_data(tileIndex, 4, blackOrbTileset);
+        break;
+    case ZELDA_TREASURE_BONE:
+        set_bkg_data(tileIndex, 4, boneTileset);
+        break;
+    case ZELDA_TREASURE_BOUQUET:
+        set_bkg_data(tileIndex, 4, bouquetTileset);
+        break;
+    case ZELDA_TREASURE_CANDLE:
+        set_bkg_data(tileIndex, 4, candleTileset);
+        break;
+    case ZELDA_TREASURE_COAL:
+        set_bkg_data(tileIndex, 4, coalTileset);
+        break;
+    case ZELDA_TREASURE_COMPASS1:
+        set_bkg_data(tileIndex, 4, compass1Tileset);
+        break;
+    case ZELDA_TREASURE_COMPASS2:
+        set_bkg_data(tileIndex, 4, compass2Tileset);
+        break;
+    case ZELDA_TREASURE_COMPASS3:
+        set_bkg_data(tileIndex, 4, compass3Tileset);
+        break;
+    case ZELDA_TREASURE_COMPASS4:
+        set_bkg_data(tileIndex, 4, compass4Tileset);
+        break;
+    case ZELDA_TREASURE_COMPASS5:
+        set_bkg_data(tileIndex, 4, compass5Tileset);
+        break;
+    case ZELDA_TREASURE_COMPASS6:
+        set_bkg_data(tileIndex, 4, compass6Tileset);
+        break;
+    case ZELDA_TREASURE_COMPASS7:
+        set_bkg_data(tileIndex, 4, compass7Tileset);
+        break;
+    case ZELDA_TREASURE_DIAMOND:
+        set_bkg_data(tileIndex, 4, diamondTileset);
+        break;
+    case ZELDA_TREASURE_EMPTYBOTTLE:
+        set_bkg_data(tileIndex, 4, emptyBottleTileset);
+        break;
+    case ZELDA_TREASURE_FLUTE:
+        set_bkg_data(tileIndex, 4, fluteTileset);
+        break;
+    case ZELDA_TREASURE_GOLDENBOOTS:
+        set_bkg_data(tileIndex, 4, goldenBootsTileset);
+        break;
+    case ZELDA_TREASURE_HARP:
+        set_bkg_data(tileIndex, 4, harpTileset);
+        break;
+    case ZELDA_TREASURE_KEY:
+        set_bkg_data(tileIndex, 4, keyTileset);
+        break;
+    case ZELDA_TREASURE_KNIFE:
+        set_bkg_data(tileIndex, 4, knifeTileset);
+        break;
+    case ZELDA_TREASURE_LADDER:
+        set_bkg_data(tileIndex, 4, ladderTileset);
+        break;
+    case ZELDA_TREASURE_LIFEPOTION:
+        set_bkg_data(tileIndex, 4, lifePotionTileset);
+        break;
+    case ZELDA_TREASURE_MAGICSHIELD:
+        set_bkg_data(tileIndex, 4, magicShieldTileset);
+        break;
+    case ZELDA_TREASURE_MAP1:
+        set_bkg_data(tileIndex, 4, map1Tileset);
+        break;
+    case ZELDA_TREASURE_MAP2:
+        set_bkg_data(tileIndex, 4, map2Tileset);
+        break;
+    case ZELDA_TREASURE_MAP3:
+        set_bkg_data(tileIndex, 4, map3Tileset);
+        break;
+    case ZELDA_TREASURE_MAP4:
+        set_bkg_data(tileIndex, 4, map4Tileset);
+        break;
+    case ZELDA_TREASURE_MAP5:
+        set_bkg_data(tileIndex, 4, map5Tileset);
+        break;
+    case ZELDA_TREASURE_MAP6:
+        set_bkg_data(tileIndex, 4, map6Tileset);
+        break;
+    case ZELDA_TREASURE_MAP7:
+        set_bkg_data(tileIndex, 4, map7Tileset);
+        break;
+    case ZELDA_TREASURE_PLANK:
+        set_bkg_data(tileIndex, 4, plankTileset);
+        break;
+    case ZELDA_TREASURE_RAFT:
+        set_bkg_data(tileIndex, 4, raftTileset);
+        break;
+    case ZELDA_TREASURE_REDBOOTS:
+        set_bkg_data(tileIndex, 4, redBootsTileset);
+        break;
+    case ZELDA_TREASURE_REDBOW:
+        set_bkg_data(tileIndex, 4, redBowTileset);
+        break;
+    case ZELDA_TREASURE_REPELLENT:
+        set_bkg_data(tileIndex, 4, repellentTileset);
+        break;
+    case ZELDA_TREASURE_RUG:
+        set_bkg_data(tileIndex, 4, rugTileset);
+        break;
+    case ZELDA_TREASURE_RUPEE:
+        set_bkg_data(tileIndex, 4, rupeeTileset);
+        break;
+    case ZELDA_TREASURE_SALTCELLAR:
+        set_bkg_data(tileIndex, 4, saltcellarTileset);
+        break;
+    case ZELDA_TREASURE_TICKETA:
+        set_bkg_data(tileIndex, 4, ticketATileset);
+        break;
+    case ZELDA_TREASURE_TICKETB:
+        set_bkg_data(tileIndex, 4, ticketBTileset);
+        break;
+    case ZELDA_TREASURE_VIALOFWIND:
+        set_bkg_data(tileIndex, 4, vialWindTileset);
+        break;
+    case ZELDA_TREASURE_WATERBOTTLE:
+        set_bkg_data(tileIndex, 4, waterBottleTileset);
+        break;
+    default:
+        break;
+    }
+}
+
+void initialiseTreasures()
+{
+    treasureTileIndex = 86;
+    slot = 0;
+    for (UINT8 i = 0; i < 8; i++)
+    {
+        if (i < totalTreasuresFound)
+        {
+            addTreasureToVram(treasures[treasureScrollOffset + i], treasureTileIndex);
+            treasureTileIndex += 4;
+
+            if (i < maxItemsOnScreen)
+            {
+                placeItemOnScreen(treasureSlots[i], treasureContainer, slot);
+                slot += 2;
+            }
+        }
+    }
+}
+
+void updateTreasures()
+{
+    slot = 0;
+    for (UINT8 i = 0; i < maxItemsOnScreen; i++)
+    {
+        // items pushed to beginning of array (3rd overwrite)
+        if (treasureScrollOffset + i >= 24)
+        {
+            placeItemOnScreen(treasureSlots[treasureScrollOffset + i - 24], treasureContainer, slot);
+        }
+        // items pushed to beginning of array (2nd overwrite)
+        else if (treasureScrollOffset + i >= 16)
+        {
+            placeItemOnScreen(treasureSlots[treasureScrollOffset + i - 16], treasureContainer, slot);
+        }
+        // items pushed to beginning of array (1st overwrite)
+        else if (treasureScrollOffset + i >= 8)
+        {
+            placeItemOnScreen(treasureSlots[treasureScrollOffset + i - 8], treasureContainer, slot);
+        }
+        else
+        {
+            placeItemOnScreen(treasureSlots[treasureScrollOffset + i], treasureContainer, slot);
+        }
+
+        slot += 2;
+    }
+
+    // update the 6 onscreen treasures
+    set_bkg_tiles(4, 11, 12, 2, treasureContainer);
+}
+
+UBYTE CanScrollTreasuresLeft()
+{
+    return treasureScrollOffset > 0;
+}
+
+UBYTE CanScrollTreasuresRight()
+{
+    return treasureScrollOffset < totalTreasuresFound - maxItemsOnScreen;
+}
+
+void ScrollTreasuresLeft()
+{
+    if (CanScrollTreasuresLeft())
+    {
+        treasureScrollOffset--;
+
+        // calculate tile index
+        treasureTileIndex = 86;
+        treasureTileIndex += (treasureScrollOffset)*4;
+
+        // check if we've written to the end of 8*4 tile slots reserved for weapons
+        if (treasureScrollOffset >= 16)
+        {
+            treasureTileIndex = 86;
+            treasureTileIndex += (treasureScrollOffset - 16) * 4;
+        }
+        else if (treasureScrollOffset >= 8)
+        {
+            treasureTileIndex = 86;
+            treasureTileIndex += (treasureScrollOffset - 8) * 4;
+        }
+
+        // add a new tile to VRAM
+        addTreasureToVram(treasures[treasureScrollOffset], treasureTileIndex);
+        placeItemOnScreen(treasureSlots[0], treasureContainer, 0);
+
+        updateTreasures();
+    }
+}
+
+void ScrollTreasuresRight()
+{
+    if (CanScrollTreasuresRight())
+    {
+        treasureScrollOffset++;
+
+        // calculate the tile index
+        treasureTileIndex = 86;
+
+        if (treasureScrollOffset > 2)
+        {
+            treasureTileIndex += (treasureScrollOffset - 2) * 4;
+        }
+
+        // check if we've written to the end of 8*4 tile slots reserved for treasures
+        if (treasureScrollOffset >= 18)
+        {
+            treasureTileIndex = 86;
+            treasureTileIndex += (treasureScrollOffset - 18) * 4;
+        }
+        else if (treasureScrollOffset >= 10)
+        {
+            treasureTileIndex = 86;
+            treasureTileIndex += (treasureScrollOffset - 10) * 4;
+        }
+
+        // add a new tile to VRAM
+        if (treasureScrollOffset > 1)
+        {
+            addTreasureToVram(treasures[treasureScrollOffset + 6], treasureTileIndex);
+            placeItemOnScreen(treasureSlots[0], treasureContainer, 0);
+        }
+
+        updateTreasures();
     }
 }
 
@@ -275,44 +575,16 @@ void main()
     // 32 tiles starting at index 22 (after the 22 core tiles)
     set_bkg_data(22, 32, celestial0tileset);
 
-    // load the weapons and treasures
-    // set_bkg_data(54, 192, inventoryTileset);
-
     // draw the background tiles
     set_bkg_tiles(0, 0, 20, 18, questStatusMap);
 
-    SHOW_BKG;
-    DISPLAY_ON;
+    // initialise the treasure tiles
+    initialiseTreasures();
+    set_bkg_tiles(4, 11, 12, 2, treasureContainer);
 
-    // update the treasure tiles
-    // populateTreasures();
-    // set_bkg_tiles(4, 11, 12, 2, treasureMap);
-
-    // update the weapon tiles
+    // initialise the weapon tiles
     initialiseWeapons();
     set_bkg_tiles(4, 15, 12, 2, weaponContainer);
-
-    // weaponMap[0] = turquoiseRingMap[0];
-    // weaponMap[1] = turquoiseRingMap[1];
-    // weaponMap[12] = turquoiseRingMap[2];
-    // weaponMap[13] = turquoiseRingMap[3];
-    // set_bkg_tiles(4, 15, 12, 2, weaponsMap);
-
-    // treasureMap[0] = compass7Map[0];
-    // treasureMap[1] = compass7Map[1];
-    // treasureMap[12] = compass7Map[2];
-    // treasureMap[13] = compass7Map[3];
-    // set_bkg_tiles(4, 11, 12, 2, treasureMap);
-
-    // load a single weapon tileset
-    // set_bkg_data(0, 4, turquoiseRingTileset);
-
-    // // load a single treasure tileset
-    // set_bkg_data(86, 4, compass7Tileset);
-
-    // // draw treasure
-    // placeItemOnScreen(treasureSlots[0], treasureContainer, 0);
-    // set_bkg_tiles(4, 11, 12, 2, treasureContainer);
 
     SHOW_BKG;
     DISPLAY_ON;
@@ -327,6 +599,14 @@ void main()
             break;
         case J_RIGHT:
             ScrollWeaponsRight();
+            delay(100);
+            break;
+        case J_UP:
+            ScrollTreasuresLeft();
+            delay(100);
+            break;
+        case J_DOWN:
+            ScrollTreasuresRight();
             delay(100);
             break;
         }
